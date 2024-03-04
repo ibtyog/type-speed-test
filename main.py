14e72e23bba7fe8f5cd821e064e030e6f0bccd63
ibtyog <piotrhirkyj@gmail.com>
2024-03-04 17:55:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import datetime
+import tkinter as tk
+import random
+
+
+with open('phrases.txt') as file:
+    phrases = file.readlines()
+
+window = tk.Tk()
+window.geometry('400x400')
+missed_chars = 0
+def text_change(*args):
+    global start
+    global missed_chars
+
+    for char in range(0,len(test_area.get())):
+        if test_area.get()[char] != title['text'][char]:
+            missed_chars += 1
+            sv.set(test_area.get()[:char])
+
+    if test_area.get() == title['text']:
+        end_text = (f'Ended in {str(datetime.datetime.now() - start)[3:]}! {missed_chars} missed characters.')
+        title['text'] = end_text
+def start_test():
+    global start
+    test_area['state'] = 'normal'
+    solution = random.choice(phrases).rstrip()
+    title['text'] = solution
+    start = datetime.datetime.now()
+sv = tk.StringVar()
+sv.trace("w", text_change)
+
+
+
+
+title = tk.Label(text="Typing speed test")
+title.pack()
+
+test_area = tk.Entry(textvariable=sv)
+test_area['state'] = 'disabled'
+test_area.pack()
+start_button = tk.Button(text="Start", command=start_test)
+start_button.pack()
+
+window.mainloop()
